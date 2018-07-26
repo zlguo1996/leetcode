3428a0a4a981382b52a81f5aa2d8e4064ca613e9
Guo Zile <1270775934@qq.com>
2018-07-26 12:09:25 +0800

    Create LeetcodeIO.hpp

---
+++ lib/LeetcodeIO.hpp
@@ -0,0 +1,77 @@
+#ifndef LeetcodeIO_hpp
+#define LeetcodeIO_hpp
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+namespace leetcodeio {
+    template <typename T>
+    inline void printVector(vector<T> vec) {
+        int i=0;
+        for(; i<vec.size()-1; i++){
+            cout << vec[i] << ",";
+        }
+        if(i<vec.size()) cout << vec[i] << endl;
+    }
+    
+    template <typename T>
+    inline void printMat(vector<vector<T>> mat) {
+        for(auto array:mat){
+            for (int i=0; i<array.size()-1; i++) {
+                cout << array[i] << ",";
+            }
+            if(array.size()!=0) cout << array.back();
+            cout << endl;
+        }
+    }
+    
+    inline vector<vector<string>> readMat(string csv_file_path) {
+        ifstream file(csv_file_path, ios::out);
+        assert(file.is_open());
+        
+        string lineStr;
+        vector<vector<string>> strMat;
+        while (getline(file, lineStr)) {
+            stringstream ss(lineStr);
+            string str;
+            
+            vector<string> strArray;
+            while (getline(ss, str, ',')) {
+                strArray.push_back(str);
+            }
+            strMat.push_back(strArray);
+        }
+        
+        return strMat;
+    }
+    
+    template <typename T>
+    inline vector<vector<T>> readMatNum(string csv_file_path) {
+        assert(sizeof(int)==sizeof(T)||sizeof(float)==sizeof(T)||sizeof(double)==sizeof(T));
+        
+        vector<vector<T>> numMat;
+        vector<vector<string>> strMat = readMat(csv_file_path);
+        
+        for(auto strArray:strMat){
+            vector<T> numArray;
+            for(auto str:strArray){
+                stringstream ss(str);
+                T num;
+                ss >> num;
+                numArray.push_back(num);
+            }
+            numMat.push_back(numArray);
+        }
+        
+        return numMat;
+    }
+}
+
+
+#endif /* LeetcodeIO_hpp */
