214a14c402db47ac7759102031a3b3474bc99885
Guo Zile <1270775934@qq.com>
2018-07-27 11:22:13 +0800

    Update #057_Insert Interval.cpp

--- algorithms/cpp/#057_Insert Interval.cpp
+++ algorithms/cpp/#057_Insert Interval.cpp
@@ -1,3 +1,4 @@
+// slow solution
 class Solution {
 public:
     struct{
@@ -24,6 +25,26 @@ public:
         new_iter->end = max((iter-1)->end, new_iter->end);
         if(iter-new_iter>1) intervals.erase(new_iter+1, iter);
         
+        return intervals;
+    }
+};
+
+// fast solution (use equal_range)
+class Solution {
+public:
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+        auto compare = [] (const Interval &intv1, const Interval &intv2)
+                          { return intv1.end < intv2.start; };
+        auto range = equal_range(intervals.begin(), intervals.end(), newInterval, compare);
+        auto itr1 = range.first, itr2 = range.second;
+        if (itr1 == itr2) {
+            intervals.insert(itr1, newInterval);
+        } else {
+            itr2--;
+            itr2->start = min(newInterval.start, itr1->start);
+            itr2->end = max(newInterval.end, itr2->end);
+            intervals.erase(itr1, itr2);
+        }
         return intervals;
     }
 };
