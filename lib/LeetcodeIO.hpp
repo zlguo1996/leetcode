19b6631b16768dc4414c387a2cf85c843f9b0740
Guo Zile <1270775934@qq.com>
2018-08-02 10:47:55 +0800

    Add files via upload

--- lib/LeetcodeIO.hpp
+++ lib/LeetcodeIO.hpp
@@ -1,3 +1,11 @@
+//
+//  LeetcodeIO.hpp
+//  leetcode
+//
+//  Created by 郭子乐 on 2018/7/26.
+//  Copyright © 2018年 郭子乐. All rights reserved.
+//
+
 #ifndef LeetcodeIO_hpp
 #define LeetcodeIO_hpp
 
@@ -9,6 +17,10 @@
 
 using namespace std;
 
+namespace test{
+    void test();
+}
+
 /*
  * Name: LeetcodeIO library
  * Function: Print vector/mat to cout, read vector/mat from csv file.
@@ -18,23 +30,23 @@ namespace leetcodeio {
     /*--------------------------print-----------------------*/
     // 打印vector<T>
     template <typename T>
-    inline void printVec(vector<T> vec) {
+    inline void printVec(vector<T> vec, string format = ",") {
         int i=0;
         for(; i<vec.size()-1; i++){
-            cout << vec[i] << ",";
+            cout << vec[i] << format;
         }
         if(i<vec.size()) cout << vec[i] << endl;
     }
     
     // 打印vector<vector<T>>
     template <typename T>
-    inline void printMat(vector<vector<T>> mat) {
+    inline void printMat(vector<vector<T>> mat, string format_ele = ",", string format_line = "\n") {
         for(auto array:mat){
             for (int i=0; i<array.size()-1; i++) {
-                cout << array[i] << ",";
+                cout << array[i] << format_ele;
             }
             if(array.size()!=0) cout << array.back();
-            cout << endl;
+            cout << format_line;
         }
     }
     
@@ -42,45 +54,14 @@ namespace leetcodeio {
     // -----vector-----
     // 从文件读取vector<string>
         // csv格式
-    inline vector<string> readVecStrCsv(string csv_file_path) {
-        ifstream file(csv_file_path, ios::out);
-        assert(file.is_open());
-        
-        string lineStr;
-        vector<string> strArray;
-        while (getline(file, lineStr)) {
-            stringstream ss(lineStr);
-            string str;
-            
-            while (getline(ss, str, ',')) {
-                strArray.push_back(str);
-            }
-        }
-        
-        return strArray;
-    }
+    vector<string> readVecStrCsv(string csv_file_path);
         // leetcode格式
-    inline vector<string> readVecStrLc(string file_path){
-        ifstream file(file_path, ios::in);
-        assert(file.is_open());
-        
-        string lineStr;
-        vector<string> vec;
-        while (getline(file, lineStr, ',')) {
-            int pos_begin=0, pos_end;
-            while(lineStr[pos_begin]=='['||lineStr[pos_begin]=='"') pos_begin++;
-            pos_end = pos_begin;
-            while(lineStr[pos_end]!='"') pos_end++;
-            vec.push_back(lineStr.substr(pos_begin, pos_end-pos_begin));
-        }
-        
-        return vec;
-    }
+    vector<string> readVecStrLc(string file_path);
     
     // 从文件读取vector<T>（T为int||float||double）
         // csv格式
     template <typename T>
-    inline vector<vector<T>> readVecNum(string csv_file_path) {
+    inline vector<T> readVecNumCsv(string csv_file_path) {
         assert(sizeof(int)==sizeof(T)||sizeof(float)==sizeof(T)||sizeof(double)==sizeof(T));
         
         vector<T> numVec;
@@ -96,18 +77,23 @@ namespace leetcodeio {
         return numVec;
     }
         // leetcode格式
-    inline vector<string> readVecNumLcCsv(string file_path){
+    template <typename T>
+    inline vector<T> readVecNumLc(string file_path){
         ifstream file(file_path, ios::in);
         assert(file.is_open());
         
         string lineStr;
-        vector<string> vec;
+        vector<T> vec;
         while (getline(file, lineStr, ',')) {
             int pos_begin=0, pos_end;
             if(lineStr[pos_begin]=='[') pos_begin++;
             pos_end = pos_begin;
             while(pos_end<lineStr.size() && lineStr[pos_end]!='"') pos_end++;
-            vec.push_back(lineStr.substr(pos_begin, pos_end-pos_begin));
+            
+            stringstream ss(lineStr.substr(pos_begin, pos_end-pos_begin));
+            T num;
+            ss >> num;
+            vec.push_back(num);
         }
         
         return vec;
@@ -116,46 +102,9 @@ namespace leetcodeio {
     // -----mat-----
     // 从文件读取vector<vector<string>>
         // csv格式
-    inline vector<vector<string>> readMatStrCsv(string csv_file_path) {
-        ifstream file(csv_file_path, ios::out);
-        assert(file.is_open());
-        
-        string lineStr;
-        vector<vector<string>> strMat;
-        while (getline(file, lineStr)) {
-            stringstream ss(lineStr);
-            string str;
-            
-            vector<string> strArray;
-            while (getline(ss, str, ',')) {
-                strArray.push_back(str);
-            }
-            strMat.push_back(strArray);
-        }
-        
-        return strMat;
-    }
+    vector<vector<string>> readMatStrCsv(string csv_file_path);
         // leetcode格式
-    inline vector<vector<string>> readMatStrLc(string file_path){
-        ifstream file(file_path, ios::in);
-        assert(file.is_open());
-        
-        string lineStr;
-        vector<vector<string>> mat;
-        while (getline(file, lineStr, ',')) {
-            if(lineStr[0]=='['){
-                vector<string> line;
-                mat.push_back(line);
-            }
-            int pos_begin=0, pos_end;
-            while(lineStr[pos_begin]=='['||lineStr[pos_begin]=='"') pos_begin++;
-            pos_end = pos_begin;
-            while(lineStr[pos_end]!='"') pos_end++;
-            mat[mat.size()-1].push_back(lineStr.substr(pos_begin, pos_end-pos_begin));
-        }
-        
-        return mat;
-    }
+    vector<vector<string>> readMatStrLc(string file_path);
     
     // 从文件读取vector<vector<T>>（T为int||float||double）
         // csv格式
